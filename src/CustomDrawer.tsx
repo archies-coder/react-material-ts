93a4f38bbc5ff4d2f709c439e3f78cac1e1cc499
Archis Bhoir <archisbhoir2016@gmail.com>
2020-08-27 22:53:19 +0530

    sales & org dropdown, home table

--- src/CustomDrawer.tsx
+++ src/CustomDrawer.tsx
@@ -1,6 +1,6 @@
 import './CustomDrawer.scss'
-import React, {FunctionComponent} from 'react';
-import {createStyles, Paper, Theme, Typography} from "@material-ui/core";
+import React, { FunctionComponent, useState } from 'react';
+import { createStyles, Paper, Theme, Typography, Collapse } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -8,9 +8,9 @@ import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
-import {makeStyles} from "@material-ui/core/styles";
-import {Link} from "react-router-dom";
-import { HomeOutlined, LibraryBooks } from '@material-ui/icons'
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { HomeOutlined, LibraryBooks, ExpandLess, ExpandMore, ChildCare } from '@material-ui/icons'
 import BusinessIcon from '@material-ui/icons/Business';
 import OrganizationIcon from "./assets/icons/OrganizationIcon";
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -39,7 +39,10 @@ const useStyles = makeStyles((theme: Theme) =>
             height: '100px',
             paddingTop: '30px',
             fontFamily: 'Poppins, sans-serif',
-        }
+        },
+        nested: {
+            paddingLeft: theme.spacing(10),
+        },
     }),
 );
 
@@ -48,59 +51,100 @@ interface OwnProps {
 
 type Props = OwnProps;
 
+type Child = {
+    title: string,
+    path?: string,
+    icon?: JSX.Element
+}
+
 
 type MappableRoutesDictionary = {
     [key: string]: {
         path: string,
-        icon:  JSX.Element
+        icon: JSX.Element,
+        children?: Child[]
     }
 }
 
 const mappableRoutes: MappableRoutesDictionary = {
     'Home': {
         path: '/',
-        icon: <HomeOutlined className="white-text"/>
+        icon: <HomeOutlined className="white-text" />
     },
     'Invites': {
         path: '/invites',
-        icon: <PersonIcon className="white-text"/>
+        icon: <PersonIcon className="white-text" />
     },
     'Employees': {
         path: '/employees',
-        icon: <OrganizationIcon className="white-text"/>
+        icon: <OrganizationIcon className="white-text" />
     },
-    'Sales & Org': {
+    'Sales & Organization': {
         path: '/sales',
-        icon: <BusinessIcon className="white-text"/>
+        icon: <BusinessIcon className="white-text" />,
+        children: [{
+            title: 'Sites'
+        }, {
+            title: 'Check in points'
+        }, {
+            title: 'organisations'
+        }]
     },
     'Reports': {
         path: '/reports',
-        icon: <LibraryBooks className="white-text"/>
+        icon: <LibraryBooks className="white-text" />
     },
     'User Management': {
         path: '/user',
-        icon: <LibraryBooks className="white-text"/>
+        icon: <LibraryBooks className="white-text" />
     },
     'Settings': {
         path: '/settings',
-        icon: <SettingsIcon className="white-text"/>
+        icon: <SettingsIcon className="white-text" />
     }
 }
 
 
 const CustomDrawer: FunctionComponent<Props> = (props) => {
     const classes = useStyles()
+
+    const [open, setOpen] = useState(false)
+
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return <Paper className={classes.paper}>
         <Typography variant="h5" className={classes.logo} noWrap>
             Company Logo
         </Typography>
-        <Divider/>
+        <Divider />
         <List>
             {Object.keys(mappableRoutes).map((key, index) => (
-                <ListItem button key={key} component={Link} to={mappableRoutes[key].path}>
-                    <ListItemIcon className="white-text">{mappableRoutes[key].icon}</ListItemIcon>
-                    <ListItemText primary={key}/>
-                </ListItem>
+                mappableRoutes[key].children ? (
+                    <>
+                        <ListItem button key={key} component={Link} to={mappableRoutes[key].path} onClick={handleClick}>
+                            <ListItemIcon className="white-text">{mappableRoutes[key].icon}</ListItemIcon>
+                            <ListItemText primary={key} />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List component="div" disablePadding>
+                                {mappableRoutes[key].children.map(child => (
+                                    <ListItem key={child.title} button className={classes.nested}>
+                                        <ListItemText primary={child.title} />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Collapse>
+                    </>
+                ) : (
+                        <ListItem button key={key} component={Link} to={mappableRoutes[key].path}>
+                            <ListItemIcon className="white-text">{mappableRoutes[key].icon}</ListItemIcon>
+                            <ListItemText primary={key} />
+                        </ListItem>
+                    )
             ))}
         </List>
     </Paper>
--- src/features/Home/HomeView.tsx
+++ src/features/Home/HomeView.tsx
@@ -1,15 +1,23 @@
 import React, {FunctionComponent} from 'react';
-import {Box, createStyles, Grid, Paper, Theme} from "@material-ui/core";
+import {Box, createStyles, Grid, Paper, Theme, Table, TableContainer, TableHead, TableRow, TableCell, TableBody} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 import NavGridContainer from "../../components/NavGridContainer";
-import {sizing} from '@material-ui/system';
 import HomeStats from "./HomeStats";
 import HomeDateDropdown from "./HomeDateDropdown";
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         paper: {
-            height: '100%'
+            backgroundColor: '#E7ECF6'
+        },
+        cell: {
+            borderBottom: 'none'
+        },
+        header: {
+            '& > *': {
+                fontWeight: '600 !important'
+            }
         }
     })
 )
@@ -21,16 +29,135 @@ type Props = OwnProps;
 
 const HomeView: FunctionComponent<Props> = (props) => {
     const classes = useStyles()
+
+    const data = {
+        name: 'Vijaya Tondon',
+        mobileNo: 9754821630,
+        personToMeet: 'Ramesh Chawla',
+        purpose: 'Meeting',
+        inTime: '11:30 am',
+        outTime: '2:30 pm',
+    }
+
     return (
         <>
-            <Grid item xs={12} style={{height: "250px", marginTop: 0}}>
-                <Paper className={classes.paper} style={{backgroundColor: "#e7ecf6"}}>
+            <Grid item xs={12} style={{height: "250px", marginTop: 0,}}>
+                <Paper className={classes.paper}>
                     <HomeDateDropdown />
                     <HomeStats />
                 </Paper>
             </Grid>
-            <Grid item xs style={{height: "100vh", marginTop: '22px'}}>
-                <Paper className={classes.paper} style={{backgroundColor: "#E7ECF6"}}>2</Paper>
+            <Grid item xs style={{height: "100%", marginTop: '22px'}}>
+                <Paper className={classes.paper}>
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow className={classes.header}>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>Visitor Name</TableCell>
+                                    <TableCell className={classes.cell}>Mobile No.</TableCell>
+                                    <TableCell className={classes.cell}>Person to meet</TableCell>
+                                    <TableCell className={classes.cell}>Purpose</TableCell>
+                                    <TableCell className={classes.cell}>In time</TableCell>
+                                    <TableCell className={classes.cell}>Out time</TableCell>
+                                    <TableCell className={classes.cell}></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell className={classes.cell}></TableCell>
+                                    <TableCell className={classes.cell}>{data.name}</TableCell>
+                                    <TableCell className={classes.cell}>{data.mobileNo}</TableCell>
+                                    <TableCell className={classes.cell}>{data.personToMeet}</TableCell>
+                                    <TableCell className={classes.cell}>{data.purpose}</TableCell>
+                                    <TableCell className={classes.cell}>{data.inTime}</TableCell>
+                                    <TableCell className={classes.cell}>{data.outTime}</TableCell>
+                                    <TableCell className={classes.cell}><MoreHorizIcon /></TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Paper>
             </Grid>
         </>
     );
