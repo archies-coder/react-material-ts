db177aa4332e47aea9abd7f485b3f05dbac6dc43
archisbhoir <archisbhoir2016@gmail.com>
2020-09-16 20:59:17 +0530

    Invite form post, date in invites view

--- src/features/Invites/InviteForm.tsx
+++ src/features/Invites/InviteForm.tsx
@@ -1,22 +1,23 @@
-import React, {FunctionComponent, useEffect, useState} from 'react';
-import {Box, createStyles, Grid, Paper} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import {Theme} from "@material-ui/core/styles/createMuiTheme";
-import {ArrowBackIos, CameraAlt} from "@material-ui/icons";
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import { Box, createStyles, Grid, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import { ArrowBackIos, CameraAlt } from "@material-ui/icons";
 import TextInput from "../../components/TextInput";
 import SelectInput from "../../components/SelectInput";
 import CustomButton from "../../components/Button";
-import {BrowserRouterProps, Redirect, RouteComponentProps} from 'react-router-dom';
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../app/rootReducer";
-import {VisitorInfo} from "../../api/Apis";
+import { BrowserRouterProps, Redirect, RouteComponentProps } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/rootReducer";
+import { apis, VisitorInfo } from "../../api/Apis";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
     paper: {
         backgroundColor: '#E7ECF6',
         borderRadius: theme.shape.borderRadius - 5,
         marginRight: 30,
-        height: '100%',
+        height: '100vh',
+        // marginTop
     },
     header: {
         fontSize: '22px',
@@ -35,38 +36,11 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
         verticalAlign: 'bottom',
         cursor: 'pointer',
     },
-    imageContainer: {
-        padding: theme.spacing(0, 0, 0, 8),
-    },
-    imageUpload: {
-        position: 'relative',
-        backgroundColor: '#fff',
-        height: '100px',
-        width: '100px',
-        textAlign: 'center',
-        borderRadius: theme.shape.borderRadius,
-        '& > svg': {
-            height: '100%',
-            opacity: 0.7,
-            fontSize: '44px'
-        }
-    },
-    visitorInfo: {
-        padding: theme.spacing(2, 0, 1, 8),
-    },
-    appointment: {
-        padding: theme.spacing(1, 0, 2, 8),
-    },
     rightInputs: {
         marginTop: 134,
     },
     button: {
         marginRight: 20
-    },
-    selectInput: {
-        '& > .makeStyles-inputContainer-32': {
-            // padding: 0
-        }
     },
     inputGrid: {
         marginTop: '30px',
@@ -92,21 +66,45 @@ const InviteForm: FunctionComponent<Props> = (props) => {
 
     const defaultInputState = {
         id: Number,
-        avatar: '',
+        time: '',
         name: '',
         mobileNo: Number,
         personToMeet: '',
         purpose: '',
-        inTime: '',
-        outTime: '',
+        email: '',
     }
 
     const [inputState, setInputState] = useState<any>(defaultInputState)
 
-    const handleChange = (e: any) => setInputState({
+    const handleChange =(e: any) => setInputState({
         ...inputState,
         [e.target.name]: e.target.value
     })
+
+    const handleSubmit = async (e: any) => {
+        const {
+            name,
+            mobileNo,
+            personToMeet,
+            purpose,
+            email,
+            time} = inputState
+
+        const response = await apis.post('/product/reception/user/invite', JSON.stringify({
+            "name": name,
+            "mobile": mobileNo,
+            "email": email,
+            "tomeet": personToMeet,
+            "purpose": purpose,
+            "scheduletime": time
+        }), {
+            headers: {
+                "Content-Type": "application/json",
+                // "Content-Length": 2617
+            },
+        })
+        if (response) setInputState(defaultInputState)
+    }
 
     // const {
     //     visitors,
@@ -125,54 +123,56 @@ const InviteForm: FunctionComponent<Props> = (props) => {
     // }, [id])
 
     return (
-        <Grid item xs={12}>
+        <Grid item style={{ height: '100%' }}>
             <Paper className={classes.paper}>
                 <form>
                     <div className={classes.header}>
-                        <ArrowBackIos className={classes.arrowBack} onClick={() => props.history.push('/')}/>
+                        <ArrowBackIos className={classes.arrowBack} onClick={() => props.history.push('/')} />
                         <span> Visitor's Details</span>
                     </div>
-                            <Box display="flex" justifyContent="flex-end">
-                                <Box className={classes.button}>
-                                    <CustomButton>Save</CustomButton>
-                                </Box>
-                            </Box>
+                    <Box display="flex" justifyContent="flex-end">
+                        <Box className={classes.button}>
+                            <CustomButton onClick={handleSubmit}>Save</CustomButton>
+                        </Box>
+                    </Box>
                     <Grid className={classes.inputGrid} container>
                         <Grid item xs={6}>
                             <div>
                                 <TextInput label="Schedule Time"
-                                           type="text"
-                                           name="outTime"
-                                           value={inputState.outTime}
-                                           onChange={handleChange}/>
+                                    type="text"
+                                    name="time"
+                                    value={inputState.time}
+                                    onChange={handleChange} />
                                 <TextInput label="Visitor Name"
-                                           name="name"
-                                           onChange={handleChange}
-                                           value={inputState.name}/>
+                                    name="name"
+                                    onChange={handleChange}
+                                    value={inputState.name} />
                                 <TextInput label="Mobile Number"
-                                           name="mobileNo"
-                                           onChange={handleChange}
-                                           value={inputState.mobileNo}/>
+                                    name="mobileNo"
+                                    onChange={handleChange}
+                                    value={inputState.mobileNo} />
                             </div>
                         </Grid>
                         <Grid item xs={6}>
                             <TextInput
                                 label="Email"
-                                onChange={(e) => console.log(e)}
-                                value=""/>
+                                onChange={handleChange}
+                                name="email"
+                                value={inputState.email} />
                             <TextInput
                                 label="Person To Meet"
-                                onChange={(e) => console.log(e)}
-                                value=""/>
+                                onChange={handleChange}
+                                name="personToMeet"
+                                value={inputState.personToMeet} />
                             <TextInput
                                 label="Purpose to visit"
                                 name="purpose"
                                 onChange={handleChange}
-                                value={inputState.purpose}/>
+                                value={inputState.purpose} />
                         </Grid>
                     </Grid>
                     <Grid container>
-                        <Grid item xs={6} style={{marginTop: '52px'}}>
+                        <Grid item xs={6} style={{ marginTop: '52px' }}>
                         </Grid>
                     </Grid>
                 </form>
